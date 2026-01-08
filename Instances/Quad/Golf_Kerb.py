934b751a2596b6b6f422b852d25758a964189d9b
Ankush <aarora@bajajauto.co.in>
2026-01-08 14:57:12 +0530

    refactor: update vehicle instance parameters for improved accuracy in Golf_Kerb model

--- Instances/Quad/Golf_Kerb.py
+++ Instances/Quad/Golf_Kerb.py
@@ -4,15 +4,14 @@ from Instances.Quad.base import Golf, MRF13570R1269S
 currveh = Golf()
 currtire = MRF13570R1269S()
 instance = Vehicle(
-    np.array([1014.24, 376.91, 989.64]), #r_A -  Top of the Kingpin
-    np.array([1073.57+18, 414.32-104, 1062.40]), # Tierod Outer Ball Joint
-    np.array([922.74, 16.50, 1097.83+25]), # Tierod Inner Ball Joint
-    np.array([1008.76, 500.00, 961.43]), # Wheel Center
-    np.array([1002.90, 385.26, 931.31]), # Bottom of the Kingpin
-    r_La = np.array([948.76, 130.00, 951.43]), # Lower A-Arm Bush Front
-    r_Lb = np.array([1100.76, 130.00, 951.43]), # Lower A-Arm Bush Rear
-    r_Ua = np.array([948.76, 100.00, 1027.43]), # Upper A-Arm Bush Front
-    r_Ub = np.array([1100.76, 100.00, 1027.43]), # Upper A-Arm Bush Rear
+    np.array([1071.5, 285.25, 1350.214]), #r_A -  Top of the Kingpin
+    np.array([936.25, 410, 960.2469]), # Tierod Outer Ball Joint
+    np.array([954.83, 176.07, 991.0]), # Tierod Inner Ball Joint
+    np.array([1000, 490, 887.2469]), # Wheel Center
+    np.array([999.9981, 411.2266, 825.4318]), # Bottom of the Kingpin
+    r_La = np.array([919.9981, 123.229, 875.5855]), # Lower A-Arm Bush Front
+    r_Lb = np.array([1079.9, 123.229, 875.5855]), # Lower A-Arm Bush Rear
+    r_strut= np.array([1021.3, 363.0444, 924.0705]), # Strut Top Mount
     GVW = 606.16,
     b = 799.72,
     CG_height = 304.33,
